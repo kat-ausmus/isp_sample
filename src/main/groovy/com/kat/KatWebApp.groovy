7d419be1843a0ccc5bdba9d5d2e4fec51dc8ef17
Katherine Unverdi <cutterin@yahoo.com>
2016-08-01 00:42:07 -0500

    more concise code
    Updated, remove some println

--- src/main/groovy/com/kat/KatWebApp.groovy
+++ src/main/groovy/com/kat/KatWebApp.groovy
@@ -24,14 +24,15 @@ class NotificationTask extends TimerTask {
     }
 
     private void notifySubscribers(){
-        String dataPlans = new File('./src/main/sample_data/plans.json').getText('UTF-8')
         String subscriptions = new File('./src/main/sample_data/subscriptions.json').getText('UTF-8')
+        String dataPlans = new File('./src/main/sample_data/plans.json').getText('UTF-8')
         String users = new File('./src/main/sample_data/users.json').getText('UTF-8')
 
         def jsonSlurper = new JsonSlurper()
         def subscriptionList = jsonSlurper.parseText(subscriptions)
         def userList = jsonSlurper.parseText(users)
         def dataPlanList = jsonSlurper.parseText(dataPlans)
+
         def subscriptionUpdated = false;
 
         subscriptionList.subscriptions.each { subscription ->
@@ -40,35 +41,36 @@ class NotificationTask extends TimerTask {
             println " notified values are $subscription.notifiedValues"
             println "Price per month threshold is $subscription.pricePerMonthThreshold"
 
-            def subscribersDataPlan = dataPlanList.plans.findAll { dataPlan ->
-                ((dataPlan.pricePerMonth <= subscription.pricePerMonthThreshold) &&
-                        (!subscription.notifiedValues.contains(dataPlan.pricePerMonth)) &&
-                        (dataPlan.type == subscription.planType)
-                        )
-            }
-
-            if(subscribersDataPlan.size() > 0){
+            def subscribersDataPlan = getListOfSubscribersDataPlan(subscription, dataPlanList)
+            if (subscribersDataPlan.size() > 0) {
                 //notify the subscriber if he/she has not been notified yet
                 def u = userList.users.find { user ->
                     user.id == subscription.userId
                 }
                 println "*** notify $u.name using $subscription.notificationMethod that the" +
                         " $subscription.planType Internet plan is less than or equal to $subscription.pricePerMonthThreshold"
-                println" --- data plans:"
-                subscribersDataPlan.each{ dataPlan ->
+                println " --- data plans:"
+                subscribersDataPlan.each { dataPlan ->
                     subscription.notifiedValues.add(dataPlan.pricePerMonth)
                     println "Data Plan is $dataPlan.name price per month is $dataPlan.pricePerMonth"
                 }
                 subscriptionUpdated = true
             }
-            println "------------------"
-            if(subscriptionUpdated){
-                new File('./src/main/sample_data/subscriptions.json').write(new JsonBuilder(subscriptionList).toPrettyString())
-            }
-
         }
 
+        if(subscriptionUpdated){
+            new File('./src/main/sample_data/subscriptions.json').write(new JsonBuilder(subscriptionList).toPrettyString())
+        }
+    }
 
+    def getListOfSubscribersDataPlan (subscription, dataPlanList){
+
+        return dataPlanList.plans.findAll { dataPlan ->
+            ((dataPlan.pricePerMonth <= subscription.pricePerMonthThreshold) &&
+                    (!subscription.notifiedValues.contains(dataPlan.pricePerMonth)) &&
+                    (dataPlan.type == subscription.planType)
+            )
+        }
     }
 }
 
