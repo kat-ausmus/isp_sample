ca1c924b2abcd436144030f5032fd9ea75009a8b
Kat Ausmus <katausmus@gmail.com>
2017-08-07 13:47:20 -0500

    Update test endpoint to use newer GetMapping notations

--- src/main/groovy/com/kat/InternetPlanService.groovy
+++ src/main/groovy/com/kat/InternetPlanService.groovy
@@ -1,14 +1,12 @@
 package com.kat
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/isp")
 class InternetPlanService {
 
-    @RequestMapping(value="/ping", method=RequestMethod.GET)
+    @GetMapping (value="/ping")
     String ping() {
         return "{ 'msg': 'ISP plan endpoint is running.' }"
     }
